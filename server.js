9b56a9aa438a24b8ec41a0b79665946f77c38152
shernk <31926953+shernk@users.noreply.github.com>
2020-12-16 22:38:26 +0700

    set up for others user

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const io = require('socket.io')(server)
+const io = require("socket.io")(server);
 const { v4: uuidv4 } = require("uuid");
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
 // view
 app.set("view engine", "ejs");
 
 // public file go here
 // javascript for the front-end in script.js file
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -19,10 +23,12 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
-io.on('connection', socket => {
-  socket.on('join-room', () => {
-    console.log('joined-room');
-  })
-})
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    // user connected to zoom with specific user's id
+    socket.to(roomId).broadcast.emit("user-connected");
+  });
+});
 
 server.listen(3000);
